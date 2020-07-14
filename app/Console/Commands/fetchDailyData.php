497217950bc88411dbf04f66964680bbf8032d0d
bayu <bayuridhoalkhifli@gmail.com>
2020-07-14 20:46:58 +0700

    Balikin ke yang lama

--- app/Console/Commands/fetchDailyData.php
+++ app/Console/Commands/fetchDailyData.php
@@ -40,29 +40,19 @@ class fetchDailyData extends Command
      */
     public function handle()
     {
-        $case = Http::get('https://services5.arcgis.com/VS6HdKS0VfIhv8Ct/arcgis/rest/services/COVID19_Indonesia_per_Provinsi/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=standard&f=pjson&token=');
-        $caseData = $case->json();
-        $positive = 0;
-        $cured = 0;
-        $death = 0;
+        $province = Http::get('https://indonesia-covid-19.mathdro.id/api/provinsi');
+        $idCase =  Http::get('https://indonesia-covid-19.mathdro.id/api/');
+        $caseData = $province->json();
+        $idCaseData = $idCase->json();
 
-        for ($i=0; $i < count($caseData["features"]); $i++) {
-            # code...
-            $positive += $caseData["features"][$i]["attributes"]["Kasus_Posi"];
-            $cured += $caseData["features"][$i]["attributes"]["Kasus_Semb"];
-            $death += $caseData["features"][$i]["attributes"]["Kasus_Meni"];
-
-            // \Log::info($positive);
-            // dd($caseData["features"][$i]["attributes"]);
-
-
+        for ($i=0; $i < count($caseData["data"]); $i++) {
             if ($i < 34) {
                 \DB::table('daily_data')
                 ->insert([
-                    'provinceCode' => $caseData["features"][$i]["attributes"]["Kode_Provi"],
-                    'positive' => $caseData["features"][$i]["attributes"]["Kasus_Posi"],
-                    'cured' => $caseData["features"][$i]["attributes"]["Kasus_Semb"],
-                    'death' => $caseData["features"][$i]["attributes"]["Kasus_Meni"],
+                    'provinceCode' => $caseData["data"][$i]["kodeProvi"],
+                    'positive' => $caseData["data"][$i]["kasusPosi"],
+                    'cured' => $caseData["data"][$i]["kasusSemb"],
+                    'death' => $caseData["data"][$i]["kasusMeni"],
                     'createdAt' => now('Asia/Jakarta')->toDateTimeString()
                 ]);
             } else {
@@ -70,21 +60,15 @@ class fetchDailyData extends Command
                 // dd("false");
                 \DB::table('daily_data')
                 ->insert([
-                    'provinceCode' => $caseData["features"][$i]["attributes"]["Kode_Provi"],
-                    'positive' => $positive,
-                    'cured' => $cured,
-                    'death' => $death,
+                    'provinceCode' => $caseData["data"][$i]["kodeProvi"],
+                    'positive' => $idCaseData["jumlahKasus"],
+                    'cured' => $idCaseData["sembuh"],
+                    'death' => $idCaseData["meninggal"],
                     'createdAt' => now('Asia/Jakarta')->toDateTimeString()
                 ]);
             }
         }
 
-
-        
-
         \Log::info("SUKSES AMBIL DATA JAM : ".now('Asia/Jakarta')->toDateTimeString());
-        // \Log::info($case->json());
-
-
     }
 }
