58270476e3b511d9f97a060fdc1868359598d414
bayu <bayuridhoalkhifli@gmail.com>
2020-08-16 22:54:38 +0700

    Change: kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('data:fetch')->everyMinute();	    
-        $schedule->command('data:fetch')->dailyAt('19:40');
+        $schedule->command('data:fetch')->twiceDaily(15, 23);
+
 
         
     }
